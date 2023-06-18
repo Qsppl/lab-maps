812d2644a308d8bf53417d37749f189201308ef2
Tomoe <4.artem.0@gmail.com>
2023-06-19 02:46:15 +0300

    try extend ymaps NS

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,23 @@
 declare var isZoomRestricted: boolean
 
 declare var isAdmin: boolean
+
+declare namespace YmapsExtendedNS {
+    class LoadingObjectManager<T = ymaps.IGeometry> implements ymaps.ICustomizable, ymaps.IEventEmitter, ymaps.IGeoObject, ymaps.IParentOnMap {
+        options: ymaps.IOptionManager;
+    
+        events: ymaps.IEventManager;
+        
+        geometry: T | null;
+        properties: ymaps.IDataManager;
+        state: ymaps.IDataManager;
+    
+        getOverlay(): Promise<ymaps.IOverlay | null>;
+    
+        getOverlaySync(): ymaps.IOverlay | null;
+    
+        getMap(): ymaps.Map;
+    }
+}
+
+declare const ymapsExt: (typeof YmapsExtendedNS & typeof ymaps)
